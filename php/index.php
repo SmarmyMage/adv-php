ec95efb1860a14f0c1c4ec7943b6d2c3d9d14906
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-02-09 15:32:09 -0600

    php index

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<var><</var>!DOCTYPE html>
+<var><</var>html lang="en">
+
+<var><</var>head>
+    <var><</var>meta charset="UTF-8">
+    <var><</var>meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <var><</var>title>Document<var><</var>/title>
+<var><</var>/head>
+
+<var><</var>body>
+    <var><</var>?= "<var><</var>h1>Hello World!<var><</var>/h1>" ?>
+    <var><</var>?= "<var><</var>h2>About Me<var><</var>/h2>" ?>
+    <var><</var>?= "<var><</var>p>I'm using this to demonstrate how to write basic web code. You must modify the content of your page for your specific needs.<var><</var>/p>" ?>
+
+<var><</var>/body>
+
+<var><</var>/html>
