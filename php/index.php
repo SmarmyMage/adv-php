f2b735c3ba4b9133ee83be937139df96aaf052e0
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-02-09 15:43:33 -0600

    index php update

--- php/index.php
+++ php/index.php
@@ -1,17 +1,17 @@
-<var><</var>!DOCTYPE html>
-<var><</var>html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-<var><</var>head>
-    <var><</var>meta charset="UTF-8">
-    <var><</var>meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <var><</var>title>Document<var><</var>/title>
-<var><</var>/head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
 
-<var><</var>body>
-    <var><</var>?= "<var><</var>h1>Hello World!<var><</var>/h1>" ?>
-    <var><</var>?= "<var><</var>h2>About Me<var><</var>/h2>" ?>
-    <var><</var>?= "<var><</var>p>I'm using this to demonstrate how to write basic web code. You must modify the content of your page for your specific needs.<var><</var>/p>" ?>
+<body>
+   <h1>Hello World!</h1>
+    <h2>About Me</h2>
+    <p>I'm using this to demonstrate how to write basic web code. You must modify the content of your page for your specific needs.</p>
 
-<var><</var>/body>
+</body>
 
-<var><</var>/html>
+</html>
