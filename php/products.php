fbc29e007243f31dc2734bb06e83fe44a55b15fa
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-03-20 15:50:22 -0500

    mvc upload

--- php/products.php
+++ php/products.php
@@ -1,38 +1,7 @@
 <?php
 
-$dsn = "mysql:
-        host=localhost;
-        dbname=sherd_MartyAllen;
-        charset=utf8;
-        port=3306";
+require "controller.php";
 
-$pdo = new PDO($dsn, "sherd_MartyAllen", "KUE7r2kX34kf3", [
-  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
+$controller = new Controller;
 
-$stmt = $pdo->query("SELECT * FROM `products`");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Products</title>
-</head>
-
-<body>
-
-  <h1>Products</h1>
-
-  <?php foreach ($products as $product) : ?>
-    <h2><?= htmlspecialchars($product["name"]) ?></h2>
-    <p><?= htmlspecialchars($product["description"]) ?></p>
-  <?php endforeach; ?>
-
-</body>
-
-</html>
+$controller->index();
