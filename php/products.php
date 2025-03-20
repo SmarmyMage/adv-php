16705739e019d7a2241d2556b8554a5582ddc978
[Anthony Reyna] <[reynaanthony164@gmail.com]>
2025-03-20 15:33:02 -0500

    products final update

--- php/products.php
+++ php/products.php
@@ -2,7 +2,7 @@
 
 $dsn = "mysql:
         host=localhost;
-        dbname=adv_php;
+        dbname=sherd_MartyAllen;
         charset=utf8;
         port=3306";
 
@@ -13,7 +13,6 @@ $pdo = new PDO($dsn, "sherd_MartyAllen", "KUE7r2kX34kf3", [
 $stmt = $pdo->query("SELECT * FROM `products`");
 
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
